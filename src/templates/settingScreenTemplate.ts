4509c909792588eff7001e998e1f23ad66bd053a
Kevin-Developer-B <kevin_breiter@web.de>
2026-04-06 10:49:57 +0200

    edit choose-box__themes width

--- src/templates/settingScreenTemplate.ts
+++ src/templates/settingScreenTemplate.ts
@@ -15,16 +15,16 @@ export function settingScreenTemplate() {
                                     <h3>Game themes</h3>
                                 </div>
                                 <div class="choose-box__themes">
-                                    <div class="theme">
+                                    <button class="theme">
                                         <span class="theme__circle-img"></span>
                                         <span class="theme__text">Code vibes theme</span>
                                         <span class="theme__line"></span>
-                                    </div>
-                                    <div class="theme">
+                                    </button>
+                                    <button class="theme">
                                         <span class="theme__circle-img"></span>
                                         <span class="theme__text">Gaming theme</span>
                                         <span class="theme__line"></span>
-                                    </div>
+                                    </button>
                                 </div>
                             </div>
                             <div class="choose-box">
@@ -33,16 +33,16 @@ export function settingScreenTemplate() {
                                     <h3>Choose player</h3>
                                 </div>
                                 <div class="choose-box__themes">
-                                    <div class="theme">
+                                    <button class="theme">
                                         <span class="theme__circle-img"></span>
                                         <span class="theme__text">Blue</span>
                                         <span class="theme__line"></span>
-                                    </div>
-                                    <div class="theme">
+                                    </button>
+                                    <button class="theme">
                                         <span class="theme__circle-img"></span>
                                         <span class="theme__text">Orange</span>
                                         <span class="theme__line"></span>
-                                    </div>
+                                    </button>
                                 </div>
                             </div>
                             <div class="choose-box">
@@ -51,21 +51,21 @@ export function settingScreenTemplate() {
                                     <h3>Board size</h3>
                                 </div>
                                 <div class="choose-box__themes">
-                                    <div class="theme">
+                                    <button class="theme">
                                         <span class="theme__circle-img"></span>
                                         <span class="theme__text">16 cards</span>
                                         <span class="theme__line"></span>
-                                    </div>
-                                    <div class="theme">
+                                    </button>
+                                    <button class="theme">
                                         <span class="theme__circle-img"></span>
                                         <span class="theme__text">24 cards</span>
                                         <span class="theme__line"></span>
-                                    </div>
-                                    <div class="theme">
+                                    </button>
+                                    <button class="theme">
                                         <span class="theme__circle-img"></span>
                                         <span class="theme__text">36 cards</span>
                                         <span class="theme__line"></span>
-                                    </div>
+                                    </button>
                                 </div>
                             </div>
                         </div>
