1a929a11aff8d4d136a67b9c32d5e982ce30a0f6
Kevin-Developer-B <kevin_breiter@web.de>
2026-04-22 17:50:56 +0200

    edit in startPage h2 and h1

--- src/templates/startPageTemplate.ts
+++ src/templates/startPageTemplate.ts
@@ -4,8 +4,8 @@ export function startPageTemplate() {
             <div class="container">
                 <div class="start-sceen-box bg-yellow-controller">
                     <div class="headline">
-                        <span class="headline__h2">It's play Time</span>
-                        <span class="headline__h1">Ready to play?</span>
+                        <h2 class="headline__h2">It's play Time</h2>
+                        <h1 class="headline__h1">Ready to play?</h1>
                     </div>
                     <button class="btn" id="playBtn">
                         <img class="btn__img--rotate" src="./assets/img/stadia_controller.png" alt="arrow-left">
